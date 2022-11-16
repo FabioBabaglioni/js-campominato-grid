f57718296318368afe7764b134a4d8e097a01442
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-11-16 16:26:12 +0100

    quadratino si colora al click

--- js/main.js
+++ js/main.js
@@ -10,4 +10,14 @@ for(let i = 0; i<100; i++){
 
 
     myElement.appendChild(nuovoElemento);
+
+
+    // click per colorare / scolorare il quadratino di blu
+    nuovoElemento.addEventListener("click",
+    
+        function(){
+            nuovoElemento.classList.toggle("click")
+        }
+    
+    );
 }
